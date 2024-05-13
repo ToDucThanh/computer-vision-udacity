5f3636bb17d57723a7011a5d331bebb34a29b49d
Jupiter <toducthanh155@gmail.com>
2024-05-13 22:46:14 +0700

    Add project materials

---
+++ Image_captioning/model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet34(pretrained=True) # I choose to use resnet 34 as suggestion
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True, dropout=0, bidirectional=False)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions):
+        embeddings = self.embed(captions[:, :-1])  # Exclude the <end> token
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
+        return outputs
+
+    def sample(self, inputs, states=None, max_len=20):
+        "accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len)"
+        predicted_sentence = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            _, predicted = outputs.max(1)
+            predicted_sentence.append(predicted.item())
+            inputs = self.embed(predicted).unsqueeze(1)
+        return predicted_sentence
---
+++ README.md
@@ -0,0 +1,11 @@
+# ComputerVision-udacity
+
+Projects done in Udacity Computer Vision Nanodegree.
+
+- Project 1: [Facical Keypoint Detection](Facial_keypoint_detection)
+- Project 2: [Image captioning](Image_captioning)
+- Project 3: [SLAM](SLAM)
+
+[Certificate](https://www.udacity.com/certificate/e/25f4f376-c192-11ee-b333-db78288f1f76?_gl=1*1ihj2qz*_ga*MTY5MjcwNTI4My4xNzA4Mjg3NTYy*_ga_CF22GKVCFK*MTcxNTYxMzQwNy4zNi4xLjE3MTU2MTQzNDIuNTIuMC4w)
+
+![Certificate](assets/certificate.png)
